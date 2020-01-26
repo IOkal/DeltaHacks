2efe42ff0ab0666b1929fb9b937ede8a671e3323
Dima Ismail <35305172+dima328@users.noreply.github.com>
2020-01-26 10:55:32 -0500

    response works !!!!

--- awstest.js
+++ awstest.js
@@ -1,10 +1,9 @@
 //Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
-    var fs = require('fs');
 
    const AWS = require('aws-sdk')
 //    const bucket = 'bucket' // the bucketname without s3://
-   const photo  = 'esha.jpg' // the name of file
+   const photo  = 'iyad.jpg' // the name of file
    const config = new AWS.Config({
     accessKeyId: 'AKIAUHXLQKOZPEG2XZD6',
     secretAccessKey: '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',
@@ -15,7 +14,7 @@
    const params = {
     Image: {"S3Object": {
         "Bucket": "dhacks-recognize",
-        "Name": "esha.jpg"
+        "Name": "iyad.jpg"
     }},
     //{Bytes: new Buffer.from(fs.readFileSync("./esha.jpg"), 'base64')}, //ArrayBuffer.from('./sampletext.txt')},
     Attributes: ['ALL']
@@ -27,42 +26,9 @@
      } else {
        console.log(`Detected faces for: ${photo}`)
        response.FaceDetails.forEach(data => {
-         let low  = data.AgeRange.Low
-         let high = data.AgeRange.High
-         console.log(`The detected face is between: ${low} and ${high} years old`)
          console.log("All other attributes:")
-         console.log(`  BoundingBox.Width:      ${data.BoundingBox.Width}`)
-         console.log(`  BoundingBox.Height:     ${data.BoundingBox.Height}`)
-         console.log(`  BoundingBox.Left:       ${data.BoundingBox.Left}`)
-         console.log(`  BoundingBox.Top:        ${data.BoundingBox.Top}`)
-         console.log(`  Age.Range.Low:          ${data.AgeRange.Low}`)
-         console.log(`  Age.Range.High:         ${data.AgeRange.High}`)
-         console.log(`  Smile.Value:            ${data.Smile.Value}`)
-         console.log(`  Smile.Confidence:       ${data.Smile.Confidence}`)
-         console.log(`  Eyeglasses.Value:       ${data.Eyeglasses.Value}`)
-         console.log(`  Eyeglasses.Confidence:  ${data.Eyeglasses.Confidence}`)
-         console.log(`  Sunglasses.Value:       ${data.Sunglasses.Value}`)
-         console.log(`  Sunglasses.Confidence:  ${data.Sunglasses.Confidence}`)
-         console.log(`  Gender.Value:           ${data.Gender.Value}`)
-         console.log(`  Gender.Confidence:      ${data.Gender.Confidence}`)
-         console.log(`  Beard.Value:            ${data.Beard.Value}`)
-         console.log(`  Beard.Confidence:       ${data.Beard.Confidence}`)
-         console.log(`  Mustache.Value:         ${data.Mustache.Value}`)
-         console.log(`  Mustache.Confidence:    ${data.Mustache.Confidence}`)
-         console.log(`  EyesOpen.Value:         ${data.EyesOpen.Value}`)
-         console.log(`  EyesOpen.Confidence:    ${data.EyesOpen.Confidence}`)
-         console.log(`  MouthOpen.Value:        ${data.MouthOpen.Value}`)
-         console.log(`  MouthOpen.Confidence:   ${data.MouthOpen.Confidence}`)
          console.log(`  Emotions[0].Type:       ${data.Emotions[0].Type}`)
          console.log(`  Emotions[0].Confidence: ${data.Emotions[0].Confidence}`)
-         console.log(`  Landmarks[0].Type:      ${data.Landmarks[0].Type}`)
-         console.log(`  Landmarks[0].X:         ${data.Landmarks[0].X}`)
-         console.log(`  Landmarks[0].Y:         ${data.Landmarks[0].Y}`)
-         console.log(`  Pose.Roll:              ${data.Pose.Roll}`)
-         console.log(`  Pose.Yaw:               ${data.Pose.Yaw}`)
-         console.log(`  Pose.Pitch:             ${data.Pose.Pitch}`)
-         console.log(`  Quality.Brightness:     ${data.Quality.Brightness}`)
-         console.log(`  Quality.Sharpness:      ${data.Quality.Sharpness}`)
          console.log(`  Confidence:             ${data.Confidence}`)
          console.log("------------")
          console.log("")
